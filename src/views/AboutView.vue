b3b7e55daa4ca996e13071ed043306b7031d6f4f
Andrei-Cristian Ene <eneandrei59@gmail.com>
2026-04-21 14:39:19 +0200

    my update to Vue-3-play

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page </h1>
+    <p>Count: <b>{{ count }}</b></p>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const count = ref(0);
+const message = ref('Hello, Vue 3!');
+
+onMounted(() => {
+  console.log('Component has been mounted!');
+  debugger;
+  if (count.value < 2) {
+    count.value++;
+  }
+});
+</script>
 
 <style>
 @media (min-width: 1024px) {
