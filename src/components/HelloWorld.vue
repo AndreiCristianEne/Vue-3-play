a3f01b029485d513abcf2ab976f71c8371031d82
Andrei-Cristian Ene <eneandrei59@gmail.com>
2026-06-20 12:06:07 +0200

    app fully hooked up with Tailwind (only utility classes used)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,16 @@
 <script setup>
+defineProps({
+  msg: String,
+})
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
+  <div>
+    <h1 class="green font-medium text-[2.6rem] relative -top-[10px] text-center lg:text-left">{{ msg }}</h1>
+    <h3 class="text-[1.2rem] text-center lg:text-left">
+      You've successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="lg:min-h-screen lg:flex lg:items-center">
     <h1>This is an about page </h1>
     <p>Count: <b>{{ count }}</b></p>
   </div>
@@ -19,13 +19,3 @@ onMounted(() => {
   }
 });
 </script>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
