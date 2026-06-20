2c4400d19e4b35d538d40f1373614f0a23181a5f
Andrei-Cristian Ene <eneandrei59@gmail.com>
2026-06-20 11:55:29 +0200

    Tailwind added (App.vue) & dynamic logic for posts with selection logic

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,3 +1,34 @@
+<script setup>
+import { reactive, onMounted } from 'vue';
+import SocialPost from './molecules/SocialPost.vue';
+
+const posts = reactive([
+  {
+    username: 'Username one',
+    userId: 'usernameID1',
+    avatarSrc: 'https://i.pravatar.cc/40',
+    post: 'This is my post'
+  },
+  {
+    username: 'Username two',
+    userId: 'usernameID2',
+    avatarSrc: 'https://i.pravatar.cc/41',
+    post: 'This is a nice post'
+  }
+]);
+</script>
+
 <template>
-  <div></div>
+  <SocialPost 
+    :username="posts[0].username"
+    :userId="posts[0].userId"
+    :avatarSrc="posts[0].avatarSrc"
+    :post="posts[0].post"
+  />
+  <SocialPost 
+    :username="posts[1].username"
+    :userId="posts[1].userId"
+    :avatarSrc="posts[1].avatarSrc"
+    :post="posts[1].post"
+  />
 </template>
---
+++ src/components/molecules/SocialPost.vue
@@ -0,0 +1,62 @@
+<script setup >
+import { onMounted, ref } from 'vue';
+
+// component state, when component is clicked
+const selected = ref(false);
+
+const props = defineProps({
+  username: String,
+  userId: String,
+  avatarSrc: String,
+  post: String
+});
+
+onMounted(() => {
+  debugger;
+  console.info(props.username);
+  console.log('Component mounted with props:', props);
+});
+</script>
+
+<template>
+  <div 
+  class="SocialPost"
+  :class="{ SocialPost__selected: selected }"
+  @click = "selected = !selected"
+  >
+    <div class="header">
+      <img class="avatar" :src="props.avatarSrc" />
+      <div class="name">{{ props.username }}</div>
+      <div class="userId">@{{ userId }}</div>
+    </div>
+    <div class="post">{{ props.post }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.SocialPost{
+  &:hover {
+    cursor: pointer;
+  }
+  
+  &__selected {
+    background-color: #faf9f5;
+    border: pink solid 1px;
+  }
+
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;;
+  }
+  .avatar {
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+  .name {
+    font-weight: bold;
+    margin-right: 8px;
+    color: white;
+  }
+}
+</style>
